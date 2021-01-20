bc8a50302c56dea6ff86aede7e0c3a6c3a432563
peterjjonas <peterjjonas@tutanota.com>
2021-01-20 16:00:19 +0000

    add heroku connect

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3003/register', {
+    fetch('https://radiant-falls-74148.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
